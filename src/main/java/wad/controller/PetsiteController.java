6bec20dda64fc5ac021c6b0499fa9021bfd1e6d4
Arpiainen Hanna K <habarpia@da5-cs-bk107-08.helsinki.fi>
2016-12-12 09:53:20 +0200

    Käyttäjä ja lemmikki aloitettu

--- src/main/java/wad/controller/PetsiteController.java
+++ src/main/java/wad/controller/PetsiteController.java
@@ -1,14 +1,12 @@
-package wad;
+package wad.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PetsiteController {
-    @RequestMapping("*")
-    @ResponseBody
+    @RequestMapping("/")
     public String home() {
-        return "Hello World!!!";
+        return "index";
     }
 }
